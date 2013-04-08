43e63c69022f9090089b2682a4a0a846902ae2d4
Andrew Patton <andrew@acusti.ca>
2013-04-08 16:17:27 -0400

    Initial commit of theme
    Includes basic typography, Susy grid with initial values, helpful
    mixins and Compass plugins, some custom functions, and a very helpful
    test page with grid background to illustrate the grid and vertical
    rhythm, as well as rendering of a wide variety of HTML elements

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>
+<footer>
+	<div id="copyright">
+		<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'forme' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); ?>
+	</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
