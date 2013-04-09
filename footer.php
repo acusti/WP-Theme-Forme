42ffc33871693ee45dabd4dc9ba3d27c486a2133
Andrew Patton <andrew@acusti.ca>
2013-04-09 12:50:56 -0400

    Simplify page structure and optimize header
    Remove div#wrapper, add 'header-menu' and 'main-menu', add useful meta
    tags to header, remove duplicate theme stylesheet include (already
    included from functions.php)

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-</div>
-<footer>
-	<div id="copyright">
-		<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'forme' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); ?>
 	</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
+	<footer>
+		<div>
+			<p id="copyright"><?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'forme' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); ?></p>
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width">
 	<title><?php wp_title(' | ', true, 'right'); ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<div id="wrapper" class="hfeed">
-		<header>
-			<div id="branding">
-				<div id="site-title"><?php if ( is_singular() ) {} else {echo '<h1>';} ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a><?php if ( is_singular() ) {} else {echo '</h1>';} ?></div>
-				<p id="site-description"><?php bloginfo( 'description' ) ?></p>
-			</div>
-			<nav>
-				<div id="search">
-					<?php get_search_form(); ?>
-				</div>
-				<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-			</nav>
-		</header>
-		<div id="container">
+	<header>
+		<hgroup id="branding">
+			<h1 id="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a></h1>
+			<h2 id="site-description"><?php bloginfo( 'description' ) ?></h2>
+		</hgroup>
+		<nav id="meta-nav">
+			<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+		</nav>
+		<nav id="main-nav">
+			<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+		</nav>
+	</header>
+	<div id="container" class="hfeed">
