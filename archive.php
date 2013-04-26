be2d61f556fe15ed86d66ffc6f7e135a533901e0
Andrew Patton <andrew@acusti.ca>
2013-04-26 15:29:44 -0400

    Code format and whitespace cleanup

--- archive.php
+++ archive.php
@@ -2,13 +2,13 @@
 <section id="content">
 	<?php the_post(); ?>
 	<?php if ( is_day() ) : ?>
-		<h1 class="page-title"><?php printf( __( 'Daily Archives: %s', 'forme' ), '<span>' . get_the_time(get_option('date_format')) . '</span>' ) ?></h1>
+		<h1 class="page-title"><?php printf( __( 'Daily Archives: %s', 'forme' ), '<span>' . get_the_time( get_option( 'date_format' ) ) . '</span>' ) ?></h1>
 	<?php elseif ( is_month() ) : ?>
-		<h1 class="page-title"><?php printf( __( 'Monthly Archives: %s', 'forme' ), '<span>' . get_the_time('F Y') . '</span>' ) ?></h1>
+		<h1 class="page-title"><?php printf( __( 'Monthly Archives: %s', 'forme' ), '<span>' . get_the_time( 'F Y' ) . '</span>' ) ?></h1>
 	<?php elseif ( is_year() ) : ?>
-		<h1 class="page-title"><?php printf( __( 'Yearly Archives: %s', 'forme' ), '<span>' . get_the_time('Y') . '</span>' ) ?></h1>
-	<?php elseif ( isset($_GET['paged']) && !empty($_GET['paged']) ) : ?>
-		<h1 class="page-title"><?php _e('Blog Archives', 'forme' ); ?></h1>
+		<h1 class="page-title"><?php printf( __( 'Yearly Archives: %s', 'forme' ), '<span>' . get_the_time( 'Y' ) . '</span>' ) ?></h1>
+	<?php elseif ( isset( $_GET['paged'] ) && ! empty( $_GET['paged'] ) ) : ?>
+		<h1 class="page-title"><?php _e( 'Blog Archives', 'forme' ); ?></h1>
 	<?php endif; ?>
 	<?php rewind_posts(); ?>
 	<?php get_template_part( 'nav', 'above' ); ?>
--- page.php
+++ page.php
@@ -4,13 +4,9 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 		<div class="entry-content">
-			<?php 
-			if ( has_post_thumbnail() ) {
-				the_post_thumbnail();
-			} 
-			?>
+			<?php  if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
 			<?php the_content(); ?>
-			<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'forme' ) . '&after=</div>') ?>
+			<?php wp_link_pages( 'before=<div class="page-link">' . __( 'Pages:', 'forme' ) . '&after=</div>' ) ?>
 			<?php edit_post_link( __( 'Edit', 'forme' ), '<div class="edit-link">', '</div>' ) ?>
 		</div>
 	</article>
