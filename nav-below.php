7e530345f62e5d0495eeeb3b85f29012b18cd119
Andrew Patton <andrew@acusti.ca>
2013-07-11 17:04:58 -0400

    Adding better pagination via WP PageNavi
    With fallback in case the plugin is not installed or enabled

--- nav-below.php
+++ nav-below.php
@@ -1,6 +1,10 @@
-<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) : ?>
-<div id="nav-below" class="navigation">
-	<div class="nav-previous"><?php next_posts_link(sprintf(__( '%s older articles', 'forme' ),'<span class="meta-nav">&larr;</span>')) ?></div>
-	<div class="nav-next"><?php previous_posts_link(sprintf(__( 'newer articles %s', 'forme' ),'<span class="meta-nav">&rarr;</span>')) ?></div>
-</div>
+<?php if ( is_paged() ) : ?>
+	<?php if ( function_exists( 'wp_pagenavi' ) ) : ?>
+		<?php wp_pagenavi(); ?>
+	<?php else: ?>
+		<div id="nav-below" class="navigation">
+			<div class="nav-previous"><?php next_posts_link( sprintf( __( '%s older articles', 'forme' ), '<span class="meta-nav">&larr;</span>') ) ?></div>
+			<div class="nav-next"><?php previous_posts_link( sprintf( __( 'newer articles %s', 'forme' ), '<span class="meta-nav">&rarr;</span>' ) ) ?></div>
+		</div>
+	<?php endif; ?>
 <?php endif; ?>
