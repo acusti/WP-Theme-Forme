05307f2136404efc30a4a3827a9d0a45cbf6cafa
Andrew Patton <andrew@acusti.ca>
2014-04-21 23:34:47 -0400

    feature: added missing IE-specific html classes

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ) ?>; charset=<?php bloginfo( 'charset' ) ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
